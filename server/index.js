7838341f7fcf66a7f4d26442bd5d95b050fa1cd9
user <user@WS-1C5BEF2AFE.belinvestbank.by>
2022-04-11 17:24:47 +0300

    feat: implemnet img save and device routing

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const path = require('path')
 
 const models = require('./models/models')
 const sequelize = require("./db");
@@ -12,6 +14,8 @@ const PORT = process.env.PORT || 5000;
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve( __dirname, "static")))
+app.use(fileUpload({}));
 app.use('/api', router);
 app.use(errorHandler)
 
